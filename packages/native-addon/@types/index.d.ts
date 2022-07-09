e47873d66b3ef880d9629dcf648dab7287d692d6
Herman Lederer <germans.lederers@gmail.com>
2022-07-09 18:28:52 +0200

    Improve native-addon types

--- packages/native-addon/@types/index.d.ts
+++ packages/native-addon/@types/index.d.ts
@@ -1,22 +1,26 @@
-type ID3Frame = [
-  string,
-  any
-];
-
-type APICFrame = [
-  'APIC',
-  {
+declare module "@metashine/native-addon" {
+  type APICFrame =     {
     MIMEType: string,
     pictureType: number,
     description: string,
-    data?: ArrayBuffer;
+    data: ArrayBuffer;
   }
-];
 
-declare type ID3Tag = ID3Frame[];
+  type ID3Text = [
+    string,
+    string
+  ];
 
-declare module "@metashine/native-addon" {
+  type ID3APIC = [
+    'APIC',
+    APICFrame
+  ];
+
+  type ID3Frame = ID3Text | ID3APIC;
+
+  type ID3Tag = ID3Frame[];
+
   function loadTag(path: string): ID3Tag;
   function updateTag(path: string, update: ID3Tag): void;
-  export { loadTag, updateTag, ID3Frame, APICFrame, ID3Tag };
+  export { loadTag, updateTag, ID3Frame, APICFrame, ID3Tag, ID3Text, ID3APIC };
 }
