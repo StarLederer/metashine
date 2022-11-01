b8e4cab3b0dc5a908f2368d25e9611edb13ba0c7
Herman Lederer <germans.lederers@gmail.com>
2022-11-01 16:41:16 +0100

    Improve base flake and migrate rust out of it

--- flake.nix
+++ flake.nix
@@ -1,65 +1,55 @@
 {
-  description = "A very basic flake";
+  description = "Metashine";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
-    rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils/master";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils }:
-  let
-    system = "x86_64-linux";
-    overlays = [ (import rust-overlay) ];
-    pkgs = import nixpkgs {
-      inherit system overlays;
-    };
-    rust = pkgs.rust-bin.stable.latest.default.override {
-      extensions = [ "rust-src" ];
-      # targets = [ "arm-unknown-linux-gnueabihf" ];
-    };
-  in {
-    devShell.${system} = pkgs.mkShell {
-      buildInputs = with pkgs; [
-        xorg.libX11
-        xorg.libXcomposite
-        xorg.libXdamage
-        xorg.libXext
-        xorg.libXfixes
-        xorg.libXrandr
-        xorg.libxcb
-        xorg.libxshmfence
-        alsaLib
-        at-spi2-core
-        nss
-        expat
-        cups
-        nspr
-        glib
-        atk
-        mesa
-        libxkbcommon
-        at-spi2-atk
-        dbus
-        libdrm
-        gtk3
-        pango
-        cairo
-        gdk-pixbuf
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in rec {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            nodejs-16_x
+            nodePackages.pnpm
+            electron_15
+          ];
 
-        # Needed to compile some of the node_modules dependencies from source
-        # autoreconfHook
-        # autoPatchelfHook
+          buildInputs = with pkgs; [
+            xorg.libX11
+            xorg.libXcomposite
+            xorg.libXdamage
+            xorg.libXext
+            xorg.libXfixes
+            xorg.libXrandr
+            xorg.libxcb
+            xorg.libxshmfence
+            alsaLib
+            at-spi2-core
+            nss
+            expat
+            cups
+            nspr
+            glib
+            atk
+            mesa
+            libxkbcommon
+            at-spi2-atk
+            dbus
+            libdrm
+            gtk3
+            pango
+            cairo
+            gdk-pixbuf
 
-        rust
-        cargo
-        rustfmt
-        rust-analyzer
-
-        nodejs-16_x
-        nodePackages.pnpm
-        electron_15
-      ];
-    };
-  };
+            # Needed to compile some of the node_modules dependencies from source
+            # autoreconfHook
+            # autoPatchelfHook
+          ];
+        };
+      }
+    );
 }
